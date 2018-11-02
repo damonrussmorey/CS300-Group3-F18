0add60c628f3eccc1b3137625880ffaa497e7b18
ogkloo <toppplel@gmail.com>
2018-11-01 18:16:37 -0700

    Added more class skeletons

--- include/DataCenter.h
+++ include/DataCenter.h
@@ -1,6 +1,42 @@
 /*
 Damon Morey, Philip Prater, Samuel Shippey, Son Vu, Yves Wienecke 2018©
 DataCenter.h
+Includes:
+	Member class definition
+	Provider class definition
+	DataCenter class definition
 */
 
 #include "defs.h"
+#include <string>
+
+struct address{
+	string streetAddress;
+	string city;
+	string state;
+	string zip;
+};
+
+class Member {
+	public:
+		Member();
+		~Member();
+	protected:
+		string name;
+		string phoneNumber;
+		address fullAddress;
+		//services list
+};
+
+class Provider {
+	public:
+		Provider();
+		~Provider();
+	protected:
+		string name;
+		string phoneNumber;
+		address fullAddress;
+		unsigned int totalConsultations;
+		unsigned int totalFee;
+		//services list
+};
